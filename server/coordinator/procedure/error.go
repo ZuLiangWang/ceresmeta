8d0ff8d363eaf0262b9de5d5f601a95d2e20277c
CooooolFrog <zuliangwanghust@gmail.com>
2023-12-18 16:21:50 +0800

    make create table idempotent

--- server/coordinator/procedure/error.go
+++ server/coordinator/procedure/error.go
@@ -20,6 +20,7 @@ import "github.com/CeresDB/ceresmeta/pkg/coderr"
 
 var (
 	ErrShardLeaderNotFound     = coderr.NewCodeError(coderr.Internal, "shard leader not found")
+	ErrShardNotMatch           = coderr.NewCodeError(coderr.Internal, "target shard not match to persis data")
 	ErrProcedureNotFound       = coderr.NewCodeError(coderr.Internal, "procedure not found")
 	ErrClusterConfigChanged    = coderr.NewCodeError(coderr.Internal, "cluster config changed")
 	ErrTableNotExists          = coderr.NewCodeError(coderr.Internal, "table not exists")
